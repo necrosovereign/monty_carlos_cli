d7ed8120965c60f83d03b138ac82dabd0a142ecd
Vladimir Kharchev <necrosovereign@gmail.com>
2024-03-19 14:55:13 +0100

    License and copyright notice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// Copyright 2024 Vladimir Kharchev
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 //! A simple CLI program that uses [`monty_carlos`] crate to run Monte-Carlo simulations.
 #![warn(clippy::pedantic)]
 use clap::{Args, Parser, ValueEnum};
